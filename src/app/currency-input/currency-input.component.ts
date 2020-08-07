e629998d909c7677006ae9583448551d01e7fe17
Zoltan <zdebre@gmail.com>
2020-08-08 04:39:00 +1200

    Add form providers

--- src/app/currency-input/currency-input.component.ts
+++ src/app/currency-input/currency-input.component.ts
@@ -1,9 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, forwardRef, OnInit } from '@angular/core';
+import { NG_VALIDATORS, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'app-currency-input',
   templateUrl: './currency-input.component.html',
-  styleUrls: ['./currency-input.component.css']
+  styleUrls: ['./currency-input.component.css'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => CurrencyInputComponent),
+      multi: true,
+    }, {
+      provide: NG_VALIDATORS,
+      useExisting: forwardRef(() => CurrencyInputComponent),
+      multi: true
+    }
+  ]
 })
 export class CurrencyInputComponent implements OnInit {
 
